2a82e373e2ebfdd9b4c0885f64c1651e9ea62480
Valerio Bruno <v.bruno@iontrading.com>
2022-12-01 21:56:32 +0100

    Day1 2022

---
+++ src/main/java/it/valeriobruno/aoc2022/CalorieCounting.java
@@ -0,0 +1,56 @@
+package it.valeriobruno.aoc2022;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class CalorieCounting {
+/*
+Optional[67622]
+201491
+ */
+   TreeSet<Integer> sortedFood = new TreeSet<>(Comparator.naturalOrder());
+
+   public void loadFile(Path path) throws IOException {
+       Scanner scanner = new Scanner(path, Charset.forName("UTF-8"));
+       int food =0;
+
+       while(scanner.hasNextLine())
+       {
+           String line = scanner.nextLine();
+           if(line.equals(""))
+           {
+               sortedFood.add(food);
+               food=0;
+               continue;
+           }
+           else{
+               food+= Integer.parseInt(line);
+           }
+           System.out.println(line);
+       }
+       scanner.close();
+   }
+
+   public int max()
+   {
+       return sortedFood.last();
+   }
+
+   public int topThree()
+   {
+       Iterator<Integer> it = sortedFood.descendingIterator();
+       return it.next()+it.next()+it.next();
+   }
+
+    public static void main(String[] args) throws Exception {
+
+        CalorieCounting d1 = new CalorieCounting();
+
+        d1.loadFile(Paths.get("elfood.txt"));
+        System.out.printf("\n%d\n",d1.max());
+        System.out.println(d1.topThree());
+    }
+}
